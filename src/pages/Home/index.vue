58d00f0d703f1f9f4f1d8f37466a45429f287d6c
ManhLe <zerokisss@gmail.com>
2021-04-17 11:37:56 +0700

    init project

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <Sidebar />
+  </v-container>
+</template>
+
+<script>
+import Sidebar from '../../components/Sidebar/Sidebar'
+export default {
+  components: {
+    Sidebar
+  }
+
+}
+</script>
+
+<style>
+
+</style>
